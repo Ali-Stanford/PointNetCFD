99cc455424c70d2d29297680ed6e982eaefb9fa6
Ali-Stanford <58951525+Ali-Stanford@users.noreply.github.com>
2022-05-01 01:21:31 -0700

    Update PointNetCFD.py

--- PointNetCFD.py
+++ PointNetCFD.py
@@ -1,13 +1,14 @@
 ##### Point-cloud deep learning for prediction of fluid flow fields on irregular geometries (supervised learning) #####
 
-#**Author:** Ali Kashefi (kashefi@stanford.edu) and Davis Rempe (drempe@stanford.edu)
-#**Description:** Implementation of PointNet for *supervised learning* of computational mechanics on domains with irregular geometries
-#**Version:** 1.0
-#**Guidance:** We recommend opening and running the code on **[Google Colab](https://research.google.com/colaboratory)** as a first try. 
+#Authors: Ali Kashefi (kashefi@stanford.edu) and Davis Rempe (drempe@stanford.edu)
+#Description: Implementation of PointNet for *supervised learning* of computational mechanics on domains with irregular geometries
+#Version: 1.0
+#Guidance: We recommend opening and running the code on **[Google Colab](https://research.google.com/colaboratory)** as a first try. 
 
 ##### Citation #####
 #If you use the code, plesae cite the following journal paper: 
-#**[A point-cloud deep learning framework for prediction of fluid flow fields on irregular geometries](https://aip.scitation.org/doi/full/10.1063/5.0033376)**
+#[A point-cloud deep learning framework for prediction of fluid flow fields on irregular geometries]
+#(https://aip.scitation.org/doi/full/10.1063/5.0033376)
 
 #@article{kashefi2021PointNetCFD, 
 #author = {Kashefi, Ali  and Rempe, Davis  and Guibas, Leonidas J. }, 
